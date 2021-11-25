1ad5a775f49e2040906f90496020ac9de82f3525
Rashad Ataf <rashadattaf@gmail.com>
2021-11-25 21:36:41 +0300

    implement all together

--- server/app.ts
+++ server/app.ts
@@ -1,22 +1,30 @@
 import express, { Application } from "express";
+import mongoose from "mongoose";
 require("dotenv").config();
 import { homeRoute } from "./routes/public/users";
 import { errorRoute } from "./routes/public/error";
 
+import ApiRoutes from "./routes/api";
+
 const app: Application = express();
 const port: string = process.env.PORT || "3500";
+const uri: string = process.env.MONGODB_URL;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api", ApiRoutes);
 app.get("/", homeRoute);
 
 app.use(errorRoute);
 
-try {
-  app.listen(port, (): void => {
-    console.log(`Connected successfully on http://localhost:${port}`);
+mongoose
+  .connect(uri)
+  .then(() =>
+    app.listen(port, () =>
+      console.log(`Server running on http://localhost:${port}`)
+    )
+  )
+  .catch((error) => {
+    throw error;
   });
-} catch (error: any) {
-  console.error(`Error occured: ${error.message}`);
-}
